503a8e2587851c8ef13da2b2d5ac3ea677a8920e
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-03-03 23:44:01 -0600

    Add registration with axios and states

--- resources/assets/js/app/auth/components/Register.vue
+++ resources/assets/js/app/auth/components/Register.vue
@@ -1,11 +1,76 @@
 <template lang="html">
-  <p>Register</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Register</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" @submit.prevent="submit">
+
+                            <div class="form-group">
+                                <label for="name" class="col-md-4 control-label">Name</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control" name="name" value="" autofocus v-model="name">
+
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="" v-model="email">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" v-model="password">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Register
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-}
-</script>
+    import { mapActions } from 'vuex'
 
-<style lang="css">
-</style>
+    export default {
+        data() {
+            return {
+                name: null,
+                email: null,
+                password: null
+            }
+        },
+        methods: {
+            ...mapActions({
+                register: 'auth/register'
+            }),
+            submit() {
+                this.register({
+                    payload: {
+                        name: this.name,
+                        email: this.email,
+                        password: this.password
+                    }
+                })
+            }
+        }
+    }
+</script>
